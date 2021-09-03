5fec84ef14283f7821a3856939c7234b1479cb68
Quốc Khải <quockhai.vn@gmail.com>
2021-09-03 10:05:45 +0700

    Sync

---
+++ BullsEye/BullsEye/BullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullsEyeApp.swift
+//  BullsEye
+//
+//  Created by Quoc Khai on 28/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct BullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Navi/Navi/NaviApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaviApp.swift
+//  Navi
+//
+//  Created by Quoc Khai on 30/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct NaviApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
